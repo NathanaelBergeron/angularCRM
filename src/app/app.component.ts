af9c09b547407f6096d8c025653e6cb0e062b2f1
Nathanael Bergeron <bergeron.nathanael@gmail.com>
2022-10-25 17:29:25 +0200

    1er cours

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularCRM';
+  title = "angularCRM";
+  message = "C'est cool.";
+  maClass = 'textRed';
+  fruits= ['Apple', 'Pear', 'Cherry'];
+
+  changeMessage(): void {
+    if(this.message === "C'est cool.") {
+      this.message= "Ça marche!";
+      this.maClass = 'textYellow';
+    } else {
+      this.message= "C'est cool.";
+      this.maClass = 'textRed';
+    }
+
+  }
 }
